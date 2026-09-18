4c6c4d15a1506174e86e67c04b6b46307c6bb857
Mokun8 <okunmilo@gmail.com>
2026-09-18 12:30:51 -0700

    practice upload

---
+++ Practice090826.java
@@ -0,0 +1,19 @@
+public class Practice090826 {
+   public static void main(String[] args) {
+      int numSiC;
+      double averageGPA;
+      boolean isHappy;
+      String teacherName;
+      //declared variables
+      
+      numSiC = 33;
+      averageGPA = 4.1;
+      isHappy = true;
+      teacherName = "Mr. Krokower";
+      //assigned values to variables
+      
+      System.out.println("STAY HEALTHY CMC!");
+      System.out.println(numSiC);
+      System.out.print(averageGPA);
+   }
+}
